a14f694590f8e401f25fe410a4c3191ccce041cd
OlgaDlzk <olga.dolzhko@gmail.com>
2023-02-12 15:11:35 -0500

    erd finished, table schemata added

---
+++ table schemata.sql
@@ -0,0 +1,51 @@
+-- Creating tables and establishing data types
+
+CREATE TABLE titles (
+  title_id VARCHAR(20) NOT NULL PRIMARY KEY,
+  title VARCHAR(200) NOT NULL
+  );
+  
+-- Drop tables
+DROP TABLE salaries;
+
+CREATE TABLE departments (
+  dept_no VARCHAR(20) NOT NULL PRIMARY KEY,
+  dept_name VARCHAR(200) NOT NULL
+  );
+
+CREATE TABLE employees (
+  emp_no INT NOT NULL PRIMARY KEY,
+  emp_title_id VARCHAR(200) NOT NULL,
+  FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+  birth_date VARCHAR (50),
+  first_name VARCHAR(200) NOT NULL,
+  last_name VARCHAR(200) NOT NULL,
+  sex VARCHAR(10),
+  hire_date VARCHAR(200) NOT NULL
+  );
+  
+CREATE TABLE dept_manager (
+  dept_no VARCHAR(20) NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+  emp_no INT NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+  );
+  
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  dept_no VARCHAR(20) NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+  );
+  
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  salary INT NOT NULL
+  );
+  
+  
+-- Query all fields from the table titles
+SELECT *
+FROM dept_manager;
+
