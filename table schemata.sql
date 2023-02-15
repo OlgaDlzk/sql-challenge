28c511b0470c2a71da188370c3b6f9f7490ef6ac
OlgaDlzk <olga.dolzhko@gmail.com>
2023-02-14 21:05:57 -0500

    fixed pk

--- table schemata.sql
+++ table schemata.sql
@@ -28,24 +28,27 @@ CREATE TABLE dept_manager (
   dept_no VARCHAR(20) NOT NULL,
   FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
   emp_no INT NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  PRIMARY KEY (dept_no, emp_no)
   );
   
 CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
   dept_no VARCHAR(20) NOT NULL,
-  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+  PRIMARY KEY (emp_no, dept_no)
   );
   
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-  salary INT NOT NULL
+  salary INT NOT NULL,
+  PRIMARY KEY (emp_no, salary)
   );
   
   
 -- Query all fields from the table dept_manager
 SELECT *
-FROM dept_manager;
+FROM dept_emp;
 
